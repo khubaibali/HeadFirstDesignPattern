b11da4b8bb9c9956b521ce4f07ae33089f61853a
Syed Khubaib Ali <khubaib94quadri@gmail.com>
2023-12-20 17:50:10 +0500

    definetion added

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -7,6 +7,8 @@ namespace StrategyPattern
     internal class Program
     {
         // strategy pattern allows to encapsulate the family of algorithms.
+        // makes them interchangeable.
+        // strategy let the algorithm vary independently from the client to have it.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
